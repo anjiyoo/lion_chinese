c7394016c3093eb80f614278853cb6547d95c755
Yoojian <soiseaubleu@gmail.com>
2024-04-18 11:05:38 +0900

    commit

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9g4dpinf&v2!0p2v(hl*ka$)f4=p=asdq!_7mxdid**z@rwbn#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
